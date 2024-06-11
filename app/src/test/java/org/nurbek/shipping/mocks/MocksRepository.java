9564180e8675ecbabca60f2846ea7acd9bfc1f3e
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-06-11 22:59:09 +0200

    feat: add mocks repository get order mock method

--- app/src/test/java/org/nurbek/shipping/mocks/MocksRepository.java
+++ app/src/test/java/org/nurbek/shipping/mocks/MocksRepository.java
@@ -2,9 +2,30 @@ package org.nurbek.shipping.mocks;
 
 import java.util.EnumMap;
 
+import org.nurbek.shipping.application.initialization.BasicModule;
 import org.nurbek.shipping.domain.enums.ItemEnum;
+import org.nurbek.shipping.domain.models.Order;
+import org.nurbek.shipping.domain.repositories.ItemRepository;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 public class MocksRepository {
+    public static Order getOrder() {
+        Injector injector = Guice.createInjector(new BasicModule());
+
+        EnumMap<ItemEnum, Integer> map = MocksRepository.getOrderMap();
+        ItemRepository itemRepository = injector.getInstance(ItemRepository.class);
+
+        Order order = new Order();
+
+        for (ItemEnum type : map.keySet()) {
+            order.addItem(itemRepository.getItem(type), map.get(type));
+        }
+
+        return order;
+    }
+
     public static EnumMap<ItemEnum, Integer> getOrderMap() {
         EnumMap<ItemEnum, Integer> map = new EnumMap<>(ItemEnum.class);
 
