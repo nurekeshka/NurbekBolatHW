a8d7f09680235bc566f61ca15b05153c6bc6d939
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-06-11 23:28:41 +0200

    feat: add shipping manager and its implementation

--- app/src/main/java/org/nurbek/shipping/application/initialization/BasicModule.java
+++ app/src/main/java/org/nurbek/shipping/application/initialization/BasicModule.java
@@ -6,6 +6,8 @@ import org.nurbek.shipping.domain.repositories.SizeRepository;
 import org.nurbek.shipping.domain.repositories.impl.ContainerRepositoryInMemoryImpl;
 import org.nurbek.shipping.domain.repositories.impl.ItemRepositoryInMemoryImpl;
 import org.nurbek.shipping.domain.repositories.impl.SizeRepositoryInMemoryImpl;
+import org.nurbek.shipping.infrastructure.managers.ShippingManager;
+import org.nurbek.shipping.infrastructure.managers.impl.ShippingManagerImpl;
 
 import com.google.inject.AbstractModule;
 
@@ -15,5 +17,6 @@ public class BasicModule extends AbstractModule {
         bind(SizeRepository.class).to(SizeRepositoryInMemoryImpl.class);
         bind(ContainerRepository.class).to(ContainerRepositoryInMemoryImpl.class);
         bind(ItemRepository.class).to(ItemRepositoryInMemoryImpl.class);
+        bind(ShippingManager.class).to(ShippingManagerImpl.class);
     }
 }
