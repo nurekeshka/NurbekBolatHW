ef1714bdb57bddce2238629555e0c0fad2c26c2b
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-06-11 20:18:43 +0200

    feat: add size enum

---
+++ app/src/main/java/org/nurbek/shipping/domain/enums/SizeEnum.java
@@ -0,0 +1,10 @@
+package org.nurbek.shipping.domain.enums;
+
+public enum SizeEnum {
+    SMALL_CONTAINER,
+    LARGE_CONTAINER,
+    LAPTOP_ITEM,
+    MOUSE_ITEM,
+    DESKTOP_ITEM,
+    SCREEN_ITEM,
+}
