f0b263dac7bd21b4981eceb2a7be29d82f0f6e09
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-06-11 22:24:50 +0200

    feat: add basic module for inverse of control

---
+++ app/src/main/java/org/nurbek/shipping/application/initialization/BasicModule.java
@@ -0,0 +1,19 @@
+package org.nurbek.shipping.application.initialization;
+
+import org.nurbek.shipping.domain.repositories.ContainerRepository;
+import org.nurbek.shipping.domain.repositories.ItemRepository;
+import org.nurbek.shipping.domain.repositories.SizeRepository;
+import org.nurbek.shipping.domain.repositories.impl.ContainerRepositoryInMemoryImpl;
+import org.nurbek.shipping.domain.repositories.impl.ItemRepositoryInMemoryImpl;
+import org.nurbek.shipping.domain.repositories.impl.SizeRepositoryInMemoryImpl;
+
+import com.google.inject.AbstractModule;
+
+public class BasicModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(SizeRepository.class).to(SizeRepositoryInMemoryImpl.class);
+        bind(ContainerRepository.class).to(ContainerRepositoryInMemoryImpl.class);
+        bind(ItemRepository.class).to(ItemRepositoryInMemoryImpl.class);
+    }
+}
