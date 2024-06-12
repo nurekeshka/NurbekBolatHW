12f3d9ece96642c91ba01738df9a9eb4d532c793
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-06-12 04:17:23 +0200

    fix: application test should test application

--- app/src/test/java/org/nurbek/shipping/application/initialization/ApplicationTest.java
+++ app/src/test/java/org/nurbek/shipping/application/initialization/ApplicationTest.java
@@ -1,31 +1,16 @@
 package org.nurbek.shipping.application.initialization;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import org.nurbek.shipping.domain.models.Order;
-import org.nurbek.shipping.infrastructure.managers.ShippingManager;
-import org.nurbek.shipping.mocks.MocksRepository;
-
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-
 public class ApplicationTest {
-    private Order order = MocksRepository.getOrder();
-    private Injector injector;
-
-    @Before
-    public void init() {
-        this.injector = Guice.createInjector(new BasicModule());
-    }
-
     @Test
     public void applicationTest() {
-        ShippingManager shippingManager = this.injector.getInstance(ShippingManager.class);
-
-        shippingManager.setOrder(order);
-        assertEquals(0, Integer.parseInt("0"), 0);
+        try {
+            Application.run();
+        } catch (Exception exception) {
+            fail();
+        }
     }
 }
