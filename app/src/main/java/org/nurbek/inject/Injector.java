922a88c0e3803e188518f4a1167568b043d9eaab
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-06-13 15:43:11 +0200

    feat: add my implementation of Injector

---
+++ app/src/main/java/org/nurbek/inject/Injector.java
@@ -0,0 +1,37 @@
+package org.nurbek.inject;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+public class Injector {
+    private final Map<Class<?>, Class<?>> map;
+
+    public Injector(Map<Class<?>, Class<?>> map) {
+        this.map = map;
+    }
+
+    public <T> T getInstance(Class<T> type) {
+        Class<?> clazz = this.map.get(type);
+
+        if (clazz == null) {
+            throw new IllegalArgumentException("No implementation registered for: " + type.getName());
+        }
+
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            Class<?>[] paramTypes = constructor.getParameterTypes();
+
+            Object[] params = new Object[paramTypes.length];
+
+            for (int i = 0; i < paramTypes.length; i++) {
+                params[i] = getInstance(paramTypes[i]);
+            }
+
+            return type.cast(constructor.newInstance(params));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException
+                | NoSuchMethodException e) {
+            throw new InjectionException("Failed to create an instance of: " + clazz.getName(), e);
+        }
+    }
+}
